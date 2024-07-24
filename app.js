31514b67bd8e07cbb6f642d3e68986e4e81cd051
snowy283 <fm11wfc@outlook.com>
2024-07-24 17:49:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,22 +12,19 @@ const client = new Banchojs.BanchoClient({
 const appPrefix = ".";
 
 client.on("PM", async ({message, user}) => {
-    // Verify that the message was not sent by us
     if (user.ircUsername === USERNAME) return;
-
-    // Check for command prefix
+    
     if (message[0] !== ".") return;
-
-    // Extract the command (the first word, lowercased)
+    
     const command = message.split(" ")[0].toLowerCase();
 
-    // Handle different commands
+    
     switch(command) {
         case appPrefix + "hello":
-            return await user.sendMessage(`Hello there, ${user.ircUsername}! My name is SnowyBot and i am coded by [fm11wfc https://osu.ppy.sh/users/35878601] B)`)
+            return await user.sendMessage(`Hello there, ${user.ircUsername}! My name is SnowyBot and i am coded by [fm11wfc https://osu.ppy.sh/users/35878601] B), btw how is your day? I hope it's good :)`)
             
     }
-}); // Add a semicolon here
+}); 
 
 
 const startOsuBot = async () => {
